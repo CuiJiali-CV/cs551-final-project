a755a3cbc39d9f79ab527d80da276ae6867bec63
Jiali Cui <jcui7@stevens.edu>
2020-12-14 22:15:42 -0500

    update args

--- args.py
+++ args.py
@@ -11,8 +11,8 @@ parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initital learning rate 1e-2,12-4,0.001')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
-# parser.add_argument('--resume', default="", type=str, metavar='PATH', help='path to latest checkpoint')
-parser.add_argument('--resume', default="./checkpoint/garbage_resnext101_model_14_92_86.pth", type=str, metavar='PATH', help='path to latest checkpoint')
+parser.add_argument('--resume', default="", type=str, metavar='PATH', help='path to latest checkpoint')
+# parser.add_argument('--resume', default="./checkpoint/garbage_resnext101_model_14_92_86.pth", type=str, metavar='PATH', help='path to latest checkpoint')
 parser.add_argument('-c', '--checkpoint', default="checkpoint", type=str, metavar='PATH',
                     help='path to save checkpoint')
 parser.add_argument('--epochs', default=100, type=int, metavar='N', help='number of total epochs to run')
